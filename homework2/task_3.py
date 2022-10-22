f98ba4c10ef4c87f09e4f45518c3a8c110edbb21
ArtemVladimirivich <slivnitsin.art@mail.ru>
2022-10-22 15:18:14 +0800

    Update task_3.py

--- homework2/task_3.py
+++ homework2/task_3.py
@@ -5,8 +5,8 @@ sum = 0
 lst = []
 
 for i in range(1, num+1):
-    lst.append((1+1/i)**i)
-    sum += (1+1/i)**i
+    lst.append((1 + 1 / i) ** i)
+    sum += (1 + 1 / i) ** i
 
 print(f'{lst} - список из {num} чисел последовательности (1 + 1 / n) ** n')
 print(f'{sum} - сумма из чисел этого списка')
