a9f97b55447fb9e7b568b2bbd27f52585e480c36
ArtemVladimirivich <slivnitsin.art@mail.ru>
2022-10-22 23:48:48 +0800

    Update task_2.py

--- homework3/task_2.py
+++ homework3/task_2.py
@@ -6,5 +6,5 @@ import random
 lst = [random.randint(0, 10) for i in range(random.randint(0, 10))]
 print('Заданный список: ', lst)
 
-new_lst = [lst[i] * lst[- i -1] for i in range((len(lst) + 1) // 2)]
+new_lst = [lst[i] * lst[-i - 1] for i in range((len(lst) + 1) // 2)]
 print('Произведение пар чисел списка:', new_lst)
