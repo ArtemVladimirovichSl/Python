9168c20984bfa2b5aba5ed5242645adf4c744349
ArtemVladimirivich <slivnitsin.art@mail.ru>
2022-11-04 19:11:58 +0800

    Update task_1.py

--- homework5/task_1.py
+++ homework5/task_1.py
@@ -1,4 +1,17 @@
 # Напишите программу, удаляющую из текста все слова, в которых присутствуют все буквы "а", "б", "в"
 
-print('Редактированный текст:', " ".join(filter(
-    lambda x: 'а' and 'в' and 'б' not in x, input('Введите текст: ').split())))
+import string
+
+text = input('Введите текст: ').split()
+
+lst = []
+for i in (text):
+    if 'а' not in i or 'б' not in i or 'в' not in i:
+        lst.append(i)
+    else:
+        elem = list(i)
+        for j in (elem [-1]):
+            if j in string.punctuation:
+                lst.append(j)
+
+print(*lst)
